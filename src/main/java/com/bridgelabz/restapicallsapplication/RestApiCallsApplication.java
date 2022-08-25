f076cd6b1547304e709a8daf17d9b366fe6e2f26
chutki12 <priyankachaudhary2068>
2022-08-25 19:23:38 +0530

    ADDED : Welcome Message

---
+++ src/main/java/com/bridgelabz/restapicallsapplication/RestApiCallsApplication.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.restapicallsapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCallsApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to messaging application demonstrates REST API calls ");
+        SpringApplication.run(RestApiCallsApplication.class, args);
+
+    }
+
+}
