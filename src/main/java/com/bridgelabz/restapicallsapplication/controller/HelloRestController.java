8210d09d9a2f2290b5bdec62a493acdf028ae482
chutki12 <priyankachaudhary2068>
2022-08-25 20:35:07 +0530

    ADDED :Used Get Request Method And Pass Name As Path Variable

--- src/main/java/com/bridgelabz/restapicallsapplication/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/restapicallsapplication/controller/HelloRestController.java
@@ -1,9 +1,6 @@
 package com.bridgelabz.restapicallsapplication.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -18,5 +15,10 @@ public class HelloRestController {
 
         return "Hello " + name + "!";
 
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name;
     }
 }
