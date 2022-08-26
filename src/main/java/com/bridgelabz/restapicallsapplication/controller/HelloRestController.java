6daf3e590e363fcb83d7b505ef73014288eef9a1
chutki12 <priyankachaudhary2068>
2022-08-26 16:17:17 +0530

    ADDED :Used Post Request Method And Pass First Name And Last Name In The Body And Params

--- src/main/java/com/bridgelabz/restapicallsapplication/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/restapicallsapplication/controller/HelloRestController.java
@@ -1,24 +1,35 @@
 package com.bridgelabz.restapicallsapplication.controller;
 
+import com.bridgelabz.restapicallsapplication.Model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
+    //localhost:8080/hello
     @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello From BridgeLabz";
     }
 
+    //localhost:8080/query?name=Priyanka
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name) {
-
         return "Hello " + name + "!";
 
     }
 
+    //localhost:8080/hello/param/Priyanka
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name;
     }
+
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " !";
+
+    }
 }
+
+
